80351363c65a2badd5e5502b827b68e01e07f5d8
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-08 19:53:01 +0300

    Show/hide new todo input field on list hover

--- src/todo-controller.js
+++ src/todo-controller.js
@@ -4,6 +4,29 @@ import reAttachEventListeners from "./reattach-event-listeners";
 import { parseISO, isToday, isTomorrow } from "date-fns";
 
 const todoController = (function () {
+  // Display new todo input field when a list is hovered over
+  function displayNewTodoInput() {
+    const todosContainers = document.querySelectorAll(".todos-container");
+
+    todosContainers.forEach((todosContainer) => {
+      const newTodoInput = todosContainer.querySelector(".new-todo-name");
+
+      // Show new todo input field when mouse is over the list
+      todosContainer.addEventListener("mouseover", () => {
+        newTodoInput.style.display = "block";
+      });
+
+      // Hide new todo input field when mouse is out of the list
+      todosContainer.addEventListener("mouseout", () => {
+        if (!newTodoInput.value) {
+          newTodoInput.style.display = "none";
+          if (newTodoInput.nextElementSibling) {
+            newTodoInput.nextElementSibling.style.display = "none";
+          }
+        }
+      });
+    });
+  }
 
   return {};
 })();
