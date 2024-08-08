506615f772262a792e1d9743c184e79c5bc6f41d
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-08 19:52:27 +0300

    Add initial todo-controller module

---
+++ src/todo-controller.js
@@ -0,0 +1,11 @@
+import ToDo from "./to-do";
+import controlProjects from "./project-controller";
+import reAttachEventListeners from "./reattach-event-listeners";
+import { parseISO, isToday, isTomorrow } from "date-fns";
+
+const todoController = (function () {
+
+  return {};
+})();
+
+export default todoController;
