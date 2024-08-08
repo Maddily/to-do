4c9be64a5804c1494ec0823eaf122ea22384ea2f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-08 19:21:38 +0300

    Create initial project-controller module

---
+++ src/project-controller.js
@@ -0,0 +1,14 @@
+import Project from "./project";
+import reAttachEventListeners from "./reattach-event-listeners";
+import ToDoList from "./to-do-list";
+import ToDo from "./to-do";
+import displayContent from "./display-content";
+import controlStorage from "./storage";
+
+const controlProjects = (function () {
+  let projects = [];
+
+  return {};
+})();
+
+export default controlProjects;
