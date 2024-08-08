1964afe98d26c4e1b15cf48950093b0061110e5c
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-08 19:41:22 +0300

    Initialize list-controller module

---
+++ src/list-controller.js
@@ -0,0 +1,10 @@
+import ToDoList from "./to-do-list";
+import controlProjects from "./project-controller";
+import reAttachEventListeners from "./reattach-event-listeners";
+
+const listController = (function () {
+
+  return {};
+})();
+
+export default listController;
