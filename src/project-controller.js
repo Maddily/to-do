51108e5bf14b0944a3e53841972c9936fce216c8
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-08 19:22:41 +0300

    Add a function to create and add new projects

--- src/project-controller.js
+++ src/project-controller.js
@@ -8,7 +8,31 @@ import controlStorage from "./storage";
 const controlProjects = (function () {
   let projects = [];
 
-  return {};
+  function createProject(projectName) {
+    // Prevent duplicate project names
+    const projectExists = controlProjects.projects.some((project) => {
+      return project.name === projectName;
+    });
+
+    if (projectExists) {
+      console.log("already exists");
+      return;
+    }
+
+    const project = new Project(projectName);
+    controlProjects.projects.push(project);
+
+    // Re-display the projects after adding a new project
+    displayProjects();
+
+    reAttachEventListeners();
+
+    return project;
+  }
+
+  return {
+    createProject
+  };
 })();
 
 export default controlProjects;
