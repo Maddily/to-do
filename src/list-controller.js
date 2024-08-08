65d1ca15066ff72f98c8dd88dd9e38bbef76afbb
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-08 19:41:59 +0300

    Add a function to show input field on project hover

--- src/list-controller.js
+++ src/list-controller.js
@@ -3,8 +3,31 @@ import controlProjects from "./project-controller";
 import reAttachEventListeners from "./reattach-event-listeners";
 
 const listController = (function () {
+  // Display new list title input field when a project is hovered over
+  function displayNewListInput() {
+    const projectContainer = document.querySelectorAll(".project-container");
 
-  return {};
+    projectContainer.forEach((projectContainer) => {
+      const newListTitleInput =
+        projectContainer.querySelector(".new-list-title");
+
+      projectContainer.addEventListener("mouseover", () => {
+        newListTitleInput.style.display = "block";
+      });
+      projectContainer.addEventListener("mouseout", () => {
+        if (!newListTitleInput.value) {
+          newListTitleInput.style.display = "none";
+          newListTitleInput.nextElementSibling.style.display = "none";
+          newListTitleInput.nextElementSibling.nextElementSibling.style.display =
+            "none";
+        }
+      });
+    });
+  }
+
+  return {
+    displayNewListInput
+  };
 })();
 
 export default listController;
