4ae6b544d05eb9500d8e6d9b2a69b3c83fbe163f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-09 09:13:15 +0300

    Handle displaying list details on selection

--- src/display-content.js
+++ src/display-content.js
@@ -3,6 +3,35 @@ import todoController from "./todo-controller";
 import listController from "./list-controller";
 
 const displayContent = (function () {
+  // Display the details of a list when a list is selected
+  function initializeListDisplay() {
+    const lists = document.querySelectorAll(".list");
+
+    lists.forEach((list) =>
+      list.addEventListener("click", () => {
+        const listId = list.dataset.id;
+        const projectId = list.parentElement.dataset.id;
+        const project = controlProjects.projects.find((project) => {
+          return project.id === projectId;
+        });
+
+        const thisList = project._toDoLists.find((list) => {
+          return list.id === listId;
+        });
+
+        if (thisList) {
+          createListContent(
+            thisList.title,
+            thisList.description,
+            thisList._toDos,
+            listId,
+            projectId
+          );
+        }
+      })
+    );
+  }
+
   return {};
 })();
 
