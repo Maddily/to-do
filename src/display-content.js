4342b546db39f575d24793cb573b449eaed21721
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-09 09:12:29 +0300

    Add display-content module with initial setup

---
+++ src/display-content.js
@@ -0,0 +1,9 @@
+import controlProjects from "./project-controller";
+import todoController from "./todo-controller";
+import listController from "./list-controller";
+
+const displayContent = (function () {
+  return {};
+})();
+
+export default displayContent;
